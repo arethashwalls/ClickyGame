1b3ec31e2d3040ce5438cf0a5bef65f209391e65
ASH Walls <admin@arethawalls.com>
2019-03-13 16:58:33 -0700

    Updates

---
+++ precache-manifest.250675b48d4fa1c4557330a912def2e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12ece92c0050f63989c0",
+    "url": "/ClickyGame/static/css/main.f6e72daf.chunk.css"
+  },
+  {
+    "revision": "12ece92c0050f63989c0",
+    "url": "/ClickyGame/static/js/main.97bf2efe.chunk.js"
+  },
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.038e769c.js"
+  },
+  {
+    "revision": "9533e37b182f24732c45",
+    "url": "/ClickyGame/static/js/2.ddfbe340.chunk.js"
+  },
+  {
+    "revision": "9da213219524d7ba8b4894269dc4f76b",
+    "url": "/ClickyGame/index.html"
+  }
+];
