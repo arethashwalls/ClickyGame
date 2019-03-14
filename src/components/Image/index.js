20735aa37e3668c900b1ade6de9ab0603b1540a0
ASH Walls <admin@arethawalls.com>
2019-03-13 17:11:34 -0700

    Trying to fix broken images

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -5,7 +5,7 @@ import './style.css';
 function Image(props) {
     const {imgAlt, imgSrc} = props.lang;
     return <Col l={3} m={4} s={6} className='lang-col grey darken-3' >
-        <img src={imgSrc} alt={imgAlt} className='lang-image responsive-img z-depth-3' onClick={() => props.clickLang(props.lang)}/>
+        <img src={'%PUBLIC_URL%' + imgSrc} alt={imgAlt} className='lang-image responsive-img z-depth-3' onClick={() => props.clickLang(props.lang)}/>
     </Col>
 }
 
